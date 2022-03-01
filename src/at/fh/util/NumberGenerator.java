83375adf970fd64dacbf060c70a9587fbb7b4718
Florian Mold <florian.mold@agnet.at>
2022-03-01 20:35:17 +0100

    feat(util): add number generator for generating numbers inside an interval

---
+++ src/at/fh/util/NumberGenerator.java
@@ -0,0 +1,18 @@
+package at.fh.util;
+
+import java.util.Random;
+
+public interface NumberGenerator {
+
+    /**
+     * Generates a random number, which is in the interval [min <= x <= max].
+     *
+     * @param min The minimum number of the generator.
+     * @param max The maximum number of the generator.
+     * @return The generated number between the boundaries.
+     */
+    static int generateNumber(int min, int max) {
+        Random rng = new Random();
+        return rng.nextInt(min, max + 1);
+    }
+}
